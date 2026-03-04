1cdb5830768de1091546b2e3d1ce2d71d1123bf2
putriyani123 <smkiu.011@gmail.com>
2026-03-04 12:05:55 +0700

    kelas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KelasController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('kelas.index');
 });
+
+/*
+|--------------------------------------------------------------------------
+| ROUTE KELAS (CRUD LENGKAP)
+|--------------------------------------------------------------------------
+*/
+Route::resource('kelas', KelasController::class);
